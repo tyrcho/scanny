1b63a016af998d5aa64cb62dd9877f2365bcf067
Jonathan Macke <jonathan.macke@yahoo.fr>
2016-04-28 23:55:43 +0200

    get product information

--- analyzers/carrefour/src/main/scala/org/scanny/carrefour/CarrefourAnalyzer.scala
+++ analyzers/carrefour/src/main/scala/org/scanny/carrefour/CarrefourAnalyzer.scala
@@ -20,15 +20,6 @@ class CarrefourAnalyzer extends Analyzer {
   def getRayonList(element: Element) : Map[String, String] = {
     val elements = element.select("a.page").asInstanceOf[ArrayList[Element]].toList
     
-    for (el <- elements){
-      val url = el.attr("href")
-      if(url.contains("tous-les-rayons")) {
-      //  println (el)
-      }
-        
-    }
-      
-    
     (for {
       el <- elements
       url = el.attr("href")
@@ -38,7 +29,31 @@ class CarrefourAnalyzer extends Analyzer {
   }
   
   def listProductsInAPage(element: Element) : List[Product] = {
-    List.empty
+    
+    val elements = element.select("div.productWrap").asInstanceOf[ArrayList[Element]].toList
+    for {
+      el <- elements
+      brand = if(!el.select("div.brand").first.childNodes.isEmpty && 
+          !el.select("div.brand").first.childNode(0).childNodes.isEmpty ) 
+              el.select("div.brand").first.childNode(0).childNode(0).attr("src") else "no brand"
+      nameElement = el.select("h3.heading").first.childNode(0).childNode(0)
+      name = if(!nameElement.childNodes.isEmpty) nameElement.childNode(0).toString else nameElement.toString
+      img = el.select(".graphic").first.childNode(0).attr("href")
+      quantite = if (el.select(".unit").first != null) el.select(".unit").first.text else "no quantite"
+      priceElement = el.select(".price").first
+      
+      price = if(priceElement!=null) {
+        if (priceElement.select(".detail").first != null) priceElement.select(".detail").first.text else "no price"
+      } else "no price"
+      
+      unitPriceElement = el.select(".unitPrice").first  
+      unitPrice = if(unitPriceElement!=null) {
+        if (unitPriceElement.select(".detail").first != null) unitPriceElement.select(".detail").first.text else "no price"
+      }  else "no price"
+      //println(s"brand: $brand, name: $name, img: $img, quantite: $quantite, price: $price")
+      
+    } yield Product(brand, name, quantite, price, unitPrice, img)
+    
   }
   
 }
